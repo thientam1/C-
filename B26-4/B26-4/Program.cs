535f266a7d1ab9bce1ea552714ed0cbdc4f0bb25
thientam1 <80733456+thientam1@users.noreply.github.com>
2022-04-26 08:21:37 +0700

    up

--- B26-4/B26-4/Program.cs
+++ B26-4/B26-4/Program.cs
@@ -14,12 +14,12 @@ namespace B26_4
             
             Animal cat = new Cat();
 
-            // hoặc:
+            
             Cat cat1 = new Cat();
 
             Cow cow = new Cow();
 
-            // Gọi các phương thức
+            
             cat.Eat148();
             cow.Eat148();
 
@@ -33,48 +33,44 @@ namespace B26_4
         }
     }
 
-    // Khai báo abstract class
+    
     public abstract class Animal
     {
-        // abstract method
+        
         public abstract void AnimalSound148();
 
-        // virtual method
+        
         public virtual void Eat148()
         {
             Console.WriteLine("Động vật ăn cỏ.");
         }
 
-        // normal method
+        
         public void Run148()
         {
             Console.WriteLine("Chạy bằng 4 chân.");
         }
     }
 
-    // Lớp Cat kế thừa lớp Animal
+    
     public class Cat : Animal
     {
-        // abstract method: bắt buộc lớp con phải override lại từ lớp cha.
-        // Khi override thì phạm vi truy cập phải giống của lớp cha: public
+        
         public override void AnimalSound148()
         {
             Console.WriteLine("Mèo meow meow.");
         }
 
-        // virtual method: việc override thì không bắt buộc.
-        // Lớp con có thể dùng luôn định nghĩa từ lớp cha nếu phù hợp,
-        // hoặc override (định nghĩa lại) nếu không phù hợp.
-        // Khi override thì phạm vi truy cập phải giống của lớp cha: public
+       
         public override void Eat148()
         {
             Console.WriteLine("Mèo ăn cá.");
         }
 
-        // normal method Run1414() thì dùng luôn của lớp cha không phải làm gì.
+       
     }
 
-    // Lớp Cow kế thừa lớp Animal
+    
     public class Cow : Animal
     {
         public override void AnimalSound148()
